ca972e292ffe4755a0e4dab8eb8473aba13d658d
gagan <gagankush26@gmail.com>
2026-02-03 02:22:19 +0530

    Sidebar Added

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans`}
       >
         <Provider>
           {children}
